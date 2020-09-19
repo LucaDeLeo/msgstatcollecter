9bbb3eb140116277ee0aa6acd7b42541f13b5e38
Kenneth Syddall <kensyd_98@hotmail.com>
2020-09-19 19:09:56 -0300

    Add "Ok" and "Cancel" butons, and I define "fname"

--- TXTloader.py
+++ TXTloader.py
@@ -3,23 +3,40 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QFileDialog, QApplication, QMainWindow
 from PyQt5.uic import loadUi
 
+
+
 class MainWindow(QDialog):
+
+
     def __init__(self):
         super(MainWindow,self).__init__()
         loadUi("gui.ui",self)
         self.browse.clicked.connect(self.browsefiles)
+        self.cancel.clicked.connect(self.LoadCancel)
+        self.ok.clicked.connect(self.LoadOk)
+
 
     def browsefiles(self):
+        global fname
         fname=QFileDialog.getOpenFileName(self, 'Open File','','TXT files (*.txt)')
         self.filename.setText(fname[0])
 
+
+    def LoadCancel(self):
+        sys.exit()
+
+
+    def LoadOk(self):
+        print(fname)
+        sys.exit()
 
 
 app=QApplication(sys.argv)
 window=MainWindow()
 widget=QtWidgets.QStackedWidget()
 widget.addWidget(window)
+widget.setWindowTitle("MsgStatCollecter")
 widget.setFixedWidth(400)
-widget.setFixedHeight(150)
+widget.setFixedHeight(180)
 widget.show()
-sys.exit(app.exec_())
+app.exec_()
