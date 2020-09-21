229f6a720bf6d5cad157e47efacb2667ba72affd
LucaDeLeo <46697542+LucaDeLeo@users.noreply.github.com>
2020-09-21 10:25:28 -0300

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,27 +1,34 @@
+import re
+import datetime
+
 chat = open("demo.txt", "r", encoding="utf-8")
+
 
 dias = []
 days_total = 0
 msgs_total = 0
 day_messages = 0
-char_index = 0
 
 for line in chat:
-    day = line.partition(",")[0]
-    tmp = line.partition(", ")[2].partition(" - ")
-    hour = tmp[0]
-    text = tmp[2]
+    part = line.partition(" - ")
+    try:
+        date = datetime.datetime.strptime(part[0], "%m/%d/%y, %I:%M %p")
+    except ValueError as err:
+        continue
+
+    day = (date.year, date.month, date.day)
     msgs_total += 1
     if not dias:
         dias.append([day,0])
     if dias[-1][0] != day:
+        dias[-1][1] = day_messages
         dias.append([day,0])
-        dias[-2][1] = day_messages
         day_messages = 0
         days_total += 1
     else:
         day_messages += 1
 
 dias[-1][1] = day_messages
+
 
 print(msgs_total / len(dias))
